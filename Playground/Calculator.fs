d2c2053a6cf8c08f3a2a1ad6337e1089f75f850a
Neils Christoffersen <neils.christoffersen@gmail.com>
2015-10-07 20:49:28 -0500

    initial commit

---
+++ Playground/Calculator.fs
@@ -0,0 +1,17 @@
+﻿namespace Neils.FSharp
+
+module Adder =
+   
+    let add x y = x + y
+
+    let add' x y = 
+        let result = 
+            add x y
+        result
+
+    let add3 x = add x 3
+
+    let add3' x = add3 x    
+
+module Exponentiator =
+    let square x = x * x
---
+++ Playground/Program.fs
@@ -0,0 +1,20 @@
+﻿module Neils.FSharp.Program
+
+open Neils.FSharp
+open Neils.FSharp.Exponentiator
+
+[<EntryPoint>]
+let main args =
+    
+    let input = 5
+    let squared = square input
+    printfn "square of %d is %d" input squared
+
+    let plus3 = Adder.add3' squared
+    printfn "%d + 3 is %d" squared plus3
+
+    printfn "<enter> to quit"
+    System.Console.ReadLine() |> ignore
+
+    0
+
