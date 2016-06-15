8f7774256ad7bde2f6c5bc2f081c7274c78ded77
Neils Christoffersen <neils@endicia.com>
2016-06-15 09:02:06 -0500

    Adding project to hold pluralsight coursework

---
+++ pluralsight/FsharpJumpstart/RecordTypes.fs
@@ -0,0 +1,19 @@
+﻿#if INTERACTIVE
+#else
+module RecordTypes
+#endif
+
+type Person = 
+    {
+        FirstName : string
+        LastName : string
+    }
+
+let person = { FirstName = "Neils" ; LastName = "Christoffersen" }
+
+printfn "%s, %s" person.LastName person.FirstName
+
+let person2 = { person with FirstName = "Fred" }
+
+let person3 = { FirstName = "Neils" ; LastName = "Christoffersen" }
+
